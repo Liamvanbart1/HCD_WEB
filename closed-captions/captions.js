9acf68ce0a22202d996527a326efb426066857f5
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2025-04-10 15:55:21 +0200

    fixed subtitles

--- closed-captions/captions.js
+++ closed-captions/captions.js
@@ -1,109 +1,57 @@
-captions[0] = ['3.8','8.1'];
-captions[1] = ['8.1','10'];
-captions[2] = ['10.8','12.5'];
-	//And blood-black nothingness began to spin
-captions[3] = ['12.5','15.6'];
-captions[4] = ['17.369','20'];
-captions[5] = ['20.1','23.3'];
-	//Fuck off, skin-job!
-captions[6] = ['23.3','25.2'];
-	
-captions[7] = ['26.8','31.6'];
-captions[8] = ['32.7','33'];
-captions[9] = ['33.2','34'];
-	//Have you ever been in an institution?
-captions[10] = ['34','36'];
-captions[11] = ['36','36.3'];
-captions[12] = ['36.5','37.3'];
-	//Do they keep you in a cell?
-captions[13] = ['37.3','38.6'];
-captions[14] = ['38.6','38.9'];
-captions[15] = ['39.1','39.9'];
-	//When you're not performing your duties, do they keep you in a little box?
-captions[16] = ['39.9','42.8'];
-captions[17] = ['42.8','43.1'];
-captions[18] = ['43.3','44.1'];
-	//Interlinked
-captions[19] = ['44','44.5'];
-captions[20] = ['45','45.7'];
-	//What's it like to hold the hand of someone you love?
-captions[21] = ['45.7','47.6'];
-captions[22] = ['47.6','48.1'];
-captions[23] = ['48.3','49'];
-	//Did they teach you how to feel, finger to finger?
-captions[24] = ['49.1','50.9'];
-captions[25] = ['50.9','51.4'];
-captions[26] = ['51.6','52.3'];
-	//Do you long for having your heart interlinked?
-captions[27] = ['52.3','54'];
-captions[28] = ['54','54.5'];
-captions[29] = ['54.7','55.4'];
-	//Do you dream about being interlinked?
-captions[30] = ['55.4','57.2'];
-captions[31] = ['57.2','58'];
-	//What's it like to hold your child in your arms?
-captions[32] = ['58','59.8'];
-captions[33] = ['59.8','60.3'];
-captions[34] = ['60.3','61'];
-	//Do you feel that there's a part of you that's missing?
-captions[35] = ['61','62.7'];
-captions[36] = ['62.7','63.2'];
-captions[37] = ['63.2','63.9'];
-	//Within cells interlinked.
-captions[38] = ['63.9','65.2'];
-captions[39] = ['65.2','66.6'];
-	//Why don't you say that three times: "within cells interlinked".
-captions[40] = ['66.6','69.1'];
-
-captions[41] = ['69.1','70.6'];
-captions[42] = ['70.6','72.2'];
-captions[43] = ['72.2','74.2'];
-	// We're done.
-captions[44] = ['77.1','78.7'];
-captions[45] = ['80','81.738'];
-captions[46] = ['82.2','84.3'];
-captions[47] = ['85.4','87'];
-
-//end manual labour
-captions[48] = ['87.68','88.18'];
-captions[49] = ['88.38','88.88'];
-// Have you ever been in an institution. Cells
-captions[50] = ['88.88','91.24'];
-captions[51] = ['91.34','91.84'];
-captions[52] = ['91.98','95.52'];
-// -Cells
-captions[53] = ['95.52','96.02'];
-captions[54] = ['96.12','100.2'];
-captions[55] = ['100.3','102.4'];
-// -Interlinked
-captions[56] = ['102.64','103.3'];
-captions[57] = ['103.3','104.38'];
-captions[58] = ['104.54','105.42'];
-// Dreadfully
-captions[59] = ['105.42','105.92'];
-captions[60] = ['106','106.5'];
-captions[61] = ['106.64','109.28'];
-// -Dreadfully
-captions[62] = ['109.28','109.8'];
-captions[63] = ['109.8','112.4'];
-captions[64] = ['112.4','113'];
-// Dreadfully distinct
-captions[65] = ['113','114.06'];
-captions[66] = ['114.12','114.98'];
-captions[67] = ['115.02','115.56'];
-// -Dark
-captions[68] = ['115.56','116.26'];
-captions[69] = ['116.26','117.4'];
-captions[70] = ['117.4','118.44'];
-// Withing one stem
-captions[71] = ['118.44','120'];
-captions[72] = ['120.46','121.28'];
-captions[73] = ['121.4','122.66'];
-// -And dreadfully distinct
-captions[74] = ['122.66','123.86'];
-captions[75] = ['123.98','124.92'];
-captions[76] = ['124.92','125.52'];
-// A tall white fountain played
-captions[77] = ['125.52','127.28'];
-captions[78] = ['128.18','130.14'];
-captions[79] = ['132.58','135'];
+captions[0] = ["00.000", "01.768"]; // Subtitle 58: "All right, Murph, give me second."
+captions[1] = ["05.088", "06.532"]; // Subtitle 59: "Third."
+captions[2] = ["08.008", "09.743"]; // Subtitle 60: "- Find a gear, dumb-ass. - Grind it!"
+captions[3] = ["09.843", "10.986"]; // Subtitle 61: "Shut it, Tom!"
+captions[4] = ["12.262", "14.765"]; // Subtitle 62: "- What'd you do, Murph? - She didn't do nothing."
+captions[5] = ["14.865", "17.367"]; // Subtitle 63: "- Blew a tire is all. - Murphy's Law."
+captions[6] = ["18.101", "19.082"]; // Subtitle 64: "Shut up!"
+captions[7] = ["19.182", "21.455"]; // Subtitle 65: "- Grab the spare, Tom. - That is the spare."
+captions[8] = ["23.982", "25.584"]; // Subtitle 66: "Get the patch kit!"
+captions[9] = ["26.777", "30.055"]; // Subtitle 67: "- How am I supposed to patch it out here? - You got to figure it out."
+captions[10] = ["30.155", "32.215"]; // Subtitle 68: "I'm not always gonna be here to help you."
+captions[11] = ["40.540", "42.359"]; // Subtitle 69: "What's going on, Murph?"
+captions[12] = ["42.459", "45.737"]; // Subtitle 70: "Why did you and Mom name me after something that's bad?"
+captions[13] = ["45.837", "49.191"]; // Subtitle 71: "- We didn't. - Murphy's Law?"
+captions[14] = ["50.050", "54.204"]; // Subtitle 72: "Murphy's Law doesn't mean that something bad will happen."
+captions[15] = ["54.304", "57.749"]; // Subtitle 73: "What it means is whatever can happen will happen."
+captions[16] = ["57.849", "60.494"]; // Subtitle 74: "And that sounded just fine with us."
+captions[17] = ["64.606", "66.249"]; // Subtitle 75: "Whoa!"
+captions[18] = ["66.933", "68.126"]; // Subtitle 76: "Get in."
+captions[19] = ["69.403", "70.921"]; // Subtitle 77: "Get in, let's go."
+captions[20] = ["72.155", "74.383"]; // Subtitle 78: "What about the flat tire?"
+captions[21] = ["77.244", "78.678"]; // Subtitle 79: "Yeah."
+captions[22] = ["92.551", "96.622"]; // Subtitle 80: "It's an Indian Air Force drone. Solar cells could power an entire farm."
+captions[23] = ["96.722", "98.281"]; // Subtitle 81: "Take the wheel, Tom."
+captions[24] = ["103.645", "105.080"]; // Subtitle 82: "Go, go, go!"
+captions[25] = ["106.565", "108.333"]; // Subtitle 83: "Keep that aimed right at it."
+captions[26] = ["116.358", "118.877"]; // Subtitle 84: "Faster, Tom. I'm losing it."
+captions[27] = ["119.244", "120.387"]; // Subtitle 85: "Right at it."
+captions[28] = ["122.088", "123.331"]; // Subtitle 86: "Stay on it."
+captions[29] = ["124.616", "125.900"]; // Subtitle 87: "Here we go."
+captions[30] = ["128.420", "129.729"]; // Subtitle 88: "Whoa."
+captions[31] = ["134.760", "136.445"]; // Subtitle 89: "Nice one, Tom."
+captions[32] = ["142.909", "144.286"]; // Subtitle 90: "Dad?"
+captions[33] = ["144.603", "147.456"]; // Subtitle 91: "I almost got it. Don't stop. Don't stop!"
+captions[34] = ["147.814", "150.467"]; // Subtitle 92: "- Dad! - Tom!"
+captions[35] = ["154.946", "157.382"]; // Subtitle 93: "You told me to keep driving."
+captions[36] = ["158.116", "162.679"]; // Subtitle 94: "Well, I guess that answers the old 'If I asked you to drive off a cliff' scenario."
+captions[37] = ["165.791", "168.727"]; // Subtitle 95: "- We lost it. - No, we didn't."
+captions[38] = ["181.306", "183.074"]; // Subtitle 96: "Want to give it a whirl?"
+captions[39] = ["188.980", "190.457"]; // Subtitle 97: "This way."
+captions[40] = ["195.737", "196.922"]; // Subtitle 98: "Go."
+captions[41] = ["197.823", "200.926"]; // Subtitle 99: "Let's lay her down right there at the edge of the reservoir."
+captions[42] = ["205.122", "206.932"]; // Subtitle 100: "Nicely done."
+captions[43] = ["209.835", "210.778"]; // Subtitle 101: "How long you think it's been up there?"
+captions[44] = ["210.878", "213.489"]; // Subtitle 102: "Delhi Mission Control went down, same as ours..."
+captions[45] = ["213.589", "215.525"]; // Subtitle 103: "- ...ten years ago. - Heh."
+captions[46] = ["215.758", "217.777"]; // Subtitle 104: "So for ten years?"
+captions[47] = ["221.638", "223.333"]; // Subtitle 105: "- Why did it come down so low? - I don't know."
+captions[48] = ["223.933", "227.419"]; // Subtitle 106: "Maybe the sun cooked its brain or it was looking for something."
+captions[49] = ["227.519", "228.871"]; // Subtitle 107: "- What? - Give me a large flatblade."
+captions[50] = ["229.836", "233.606"]; // Subtitle 108: "Maybe some kind of signal? I don't know."
+captions[51] = ["237.813", "239.500"]; // Subtitle 109: "What are you gonna do with it?"
+captions[52] = ["239.702", "242.850"]; // Subtitle 110: "I'm gonna give it something socially responsible to do..."
+captions[53] = ["244.850", "246.012"]; // Subtitle 111: "like drive a combine."
+captions[54] = ["246.296", "248.098"]; // Subtitle 112: "Can't we just let it go?"
+captions[55] = ["250.292", "252.060"]; // Subtitle 113: "It wasn't hurting anybody."
+captions[56] = ["253.628", "257.024"]; // Subtitle 114: "This thing needs to learn how to adapt, Murph... like the rest of us."
