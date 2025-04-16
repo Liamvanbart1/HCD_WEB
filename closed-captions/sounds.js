81cec82e309eeb56fb505db19eb0df79a52077b8
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2025-04-16 12:15:46 +0200

    closed captions

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,12 +1,12 @@
 sounds = [
-  [0, 2],
-  [9, 10],
-  [12, 14],
-  [15, 16],
-  [16.5, 18],
-  [24, 26],
-  [25.8, 28],
-  [31, 32],
+  [30, 150], //piano muziek
+  [64, 67], // achtergrond wit door vliegtuig
+  [11, 13], // band die knapt
+  [85, 90], // geluid van auto die door maisveld rijdt en mais raakt
+  [115, 118], // computer geluiden
+  [137, 139], //auto door hek
+  [151, 153], //piano muziek stopt
+  [30, 32],
   [32, 33],
   [34, 36],
   [86, 90],
